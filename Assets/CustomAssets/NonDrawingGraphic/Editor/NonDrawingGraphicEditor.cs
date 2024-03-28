9cae50260c6632828d801695c9b336c6d6edd5f9
Inheo <agch777505@gmail.com>
2024-03-28 11:53:27 +0300

    added non draw graphics script

---
+++ Assets/CustomAssets/NonDrawingGraphic/Editor/NonDrawingGraphicEditor.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+namespace CustomAssets.NonDrawingGraphic
+{
+    //Don't forget to put this file inside a 'Editor' folder
+    [CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.serializedObject.Update();
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(base.m_Script, new GUILayoutOption[0]);
+            EditorGUI.EndDisabledGroup();
+            // skipping AppearanceControlsGUI
+            base.RaycastControlsGUI();
+            base.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
